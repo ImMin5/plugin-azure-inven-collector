f90087b51c04c2f57d725cfe0e775fff27b9bc01
MINHO KIM <mino@mz.co.kr>
2022-10-17 18:13:33 +0900

    fix: fix typo

--- src/spaceone/inventory/model/postgresql_servers/cloud_service.py
+++ src/spaceone/inventory/model/postgresql_servers/cloud_service.py
@@ -20,7 +20,7 @@ postgresql_servers_info_meta = ItemDynamicLayout.set_fields('PostgreSQL Servers'
         'safe': ['Ready'],
         'warning': ['Disabled', 'Dropping', 'Inaccessible']
     }),
-    TextDyField.data_source('Region', 'data.location'),
+    TextDyField.data_source('Location', 'data.location'),
     TextDyField.data_source('Subscription', 'data.subscription_name'),
     TextDyField.data_source('Subscription ID', 'account'),
     TextDyField.data_source('Server Name', 'data.fully_qualified_domain_name'),
